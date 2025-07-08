9f574a9b16ec272c9b08c327d21e3b454f2abdeb
aadya2775 <aadya2775@gmail.com>
2025-07-09 00:16:30 +0530

    layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,15 +1,15 @@
-// import { isAuthenticated } from '@/lib/actions/auth.action';
-// import { redirect } from 'next/navigation';
-// import React, { ReactNode } from 'react'
+import { isAuthenticated } from '@/lib/actions/auth.action';
+import { redirect } from 'next/navigation';
+import React, { ReactNode } from 'react'
 
-// const Authlayout = async ({ children }: {children : ReactNode}) => {
-//   const isUserAuthenticated = await isAuthenticated();
+const Authlayout = async ({ children }: {children : ReactNode}) => {
+  const isUserAuthenticated = await isAuthenticated();
   
-//     if(!isUserAuthenticated) redirect('/');
+    if(!isUserAuthenticated) redirect('/');
   
-//   return (
-//     <div className='auth-layout'>{children}</div>
-//   )
-// }
+  return (
+    <div className='auth-layout'>{children}</div>
+  )
+}
 
-// export default Authlayout
+export default Authlayout
