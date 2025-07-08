0385cc4632f3ec7e3601fabf7e1d25a35ad6cd95
aadya2775 <aadya2775@gmail.com>
2025-07-09 00:17:00 +0530

    layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,25 +1,25 @@
-// import Link from "next/link";
-// import React, { ReactNode } from "react";
-// import Image from "next/image";
-// import { isAuthenticated } from "@/lib/actions/auth.action";
-// import { redirect } from "next/navigation";
+import Link from "next/link";
+import React, { ReactNode } from "react";
+import Image from "next/image";
+import { isAuthenticated } from "@/lib/actions/auth.action";
+import { redirect } from "next/navigation";
 
-// const Rootlayout = async ({ children }: { children: ReactNode }) => {
-//   const isUserAuthenticated = await isAuthenticated();
+const Rootlayout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
 
-//   if(!isUserAuthenticated) redirect('/sign-in');
+  if(!isUserAuthenticated) redirect('/sign-in');
 
-//   return (
-//     <div className="root-layout">
-//       <nav>
-//         <Link href="/" className="flex items-center gap-2">
-//           <Image src="/logo.svg" alt="logo" width={38} height={32} />
-//           <h2 className="text-primary-100">placement.AI</h2>
-//         </Link>
-//       </nav>
-//       {children}
-//     </div>
-//   );
-// };
+  return (
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="logo" width={38} height={32} />
+          <h2 className="text-primary-100">placement.AI</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
+};
 
-// export default Rootlayout;
+export default Rootlayout;
