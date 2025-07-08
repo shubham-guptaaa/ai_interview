28afd26c8d4807a92264d99804f3e83f581c5c03
Shubham Gupta <153124453+shubham-guptaaa@users.noreply.github.com>
2025-07-09 00:03:43 +0530

    layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,25 @@
-import Link from "next/link";
-import React, { ReactNode } from "react";
-import Image from "next/image";
+// import Link from "next/link";
+// import React, { ReactNode } from "react";
+// import Image from "next/image";
+// import { isAuthenticated } from "@/lib/actions/auth.action";
+// import { redirect } from "next/navigation";
 
-const Rootlayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <div className="root-layout">
-      <nav>
-        <Link href="/" className="flex items-center gap-2">
-          <Image src="/logo.svg" alt="logo" width={38} height={32} />
-          <h2 className="text-primary-100">placement.AI</h2>
-        </Link>
-      </nav>
-      {children}
-    </div>
-  );
-};
+// const Rootlayout = async ({ children }: { children: ReactNode }) => {
+//   const isUserAuthenticated = await isAuthenticated();
 
-export default Rootlayout;
+//   if(!isUserAuthenticated) redirect('/sign-in');
+
+//   return (
+//     <div className="root-layout">
+//       <nav>
+//         <Link href="/" className="flex items-center gap-2">
+//           <Image src="/logo.svg" alt="logo" width={38} height={32} />
+//           <h2 className="text-primary-100">placement.AI</h2>
+//         </Link>
+//       </nav>
+//       {children}
+//     </div>
+//   );
+// };
+
+// export default Rootlayout;
